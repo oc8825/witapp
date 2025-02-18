a237927199212b3146ff2621210a611ada26d869
Owen Clarke <oc8825@princeton.edu>
2025-02-18 11:38:52 -0500

    Changing what file you find background styling in from globals.scss to backgroundStyles.module.scss

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,33 @@
 
 import Countdown from "@/components/Countdown";
 import Link from "next/link";
-import styles from "@/styles/buttons.module.scss";
+import buttonStyles from "@/styles/buttons.module.scss";
+import backgroundStyles from "@/styles/backgroundStyles.module.scss";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-   <div className="tinted-background">
+   <div className={backgroundStyles["tinted-background"]}>
       <Countdown />
       <Link href="/enterNow">
-        <button className={styles["larger-button"]} onClick={() => router.push("/enterNow")}>
+        <button className={buttonStyles["larger-button"]} onClick={() => router.push("/enterNow")}>
           ENTER NOW
         </button>
       </Link>
       <Link href="/faq">
-        <button className={styles["simple-button"]} onClick={() => router.push("/faq")}>
+        <button className={buttonStyles["simple-button"]} onClick={() => router.push("/faq")}>
           FAQ
         </button>
       </Link>
       <Link href="/privacyPolicy">
-        <button className={styles["simple-button"]} onClick={() => router.push("/privacyPolicy")}>
+        <button className={buttonStyles["simple-button"]} onClick={() => router.push("/privacyPolicy")}>
           Privacy Policy
         </button>
       </Link>
       <Link href="/termsOfService">
-        <button className={styles["simple-button"]} onClick={() => router.push("/termsOfService")}>
+        <button className={buttonStyles["simple-button"]} onClick={() => router.push("/termsOfService")}>
           Terms of Service
         </button>
       </Link>
