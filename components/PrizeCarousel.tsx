a3e83a3e877256e7b23325f8057380c38554b4fd
Owen Clarke <oc8825@princeton.edu>
2025-02-19 16:38:16 -0500

    background rectangle and automatic switching on prize carousel

--- components/PrizeCarousel.tsx
+++ components/PrizeCarousel.tsx
@@ -3,30 +3,19 @@ import styles from "../styles/prizeCarousel.module.scss";
 
 const PrizeCarousel = () => {
     return (
-        <Carousel interval = {5000}>
-            <Carousel.Item>
-                <img
-                    className={styles['prize-image']}
-                    src="/images/carousel1.png"
-                    alt="First Prize"
-                />
-                <Carousel.Caption>
-                    <h3>First Prize</h3>
-                    <p>First prize caption</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-
-            <Carousel.Item>
-                <img
-                    className={styles['prize-image']}
-                    src="/images/carousel2.png"
-                    alt="Second Prize"
-                    />
-                <Carousel.Caption>
-                    <h3>Second Prize</h3>
-                    <p>Second prize caption</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+        <Carousel interval={5000} controls={true} indicators={true} pause={false}>
+            {[1, 2, 3, 4, 5].map((num) => ( 
+                <Carousel.Item key={num}> 
+                    <div className={styles["carousel-container"]}>
+                        <img
+                            className={styles["prize-image"]}
+                            src={`/images/carousel${num}.png`}
+                            alt={`Prize ${num}`}
+                        />
+                        <p className={styles["prize-caption"]}>Prize {num} caption</p>
+                    </div>
+                </Carousel.Item>
+            ))}
         </Carousel>
     );
 };
