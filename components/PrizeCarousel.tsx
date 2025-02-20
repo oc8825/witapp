598301d8ed5d23d78351f38f2be3c75e6765c537
Owen Clarke <oc8825@princeton.edu>
2025-02-20 15:40:46 -0500

    Fixing bug where it would skip the second slide

--- components/PrizeCarousel.tsx
+++ components/PrizeCarousel.tsx
@@ -4,17 +4,24 @@ import { useState, useEffect } from "react";
 
 const PrizeCarousel = () => {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [isFirstCycle, setIsFirstCycle] = useState(true);
 
     useEffect(() => {
+        /* The first cycle was skipping the second slide. Make first cycle slightly
+        longer to allow all slides to be recognized properly before starting
+        automatic transitions */
+        const startDelay = isFirstCycle ? 5100 : 5000;
         const interval = setInterval(() => {
             setActiveIndex((prevIndex) => (prevIndex + 1) % 5);
-        }, 5000);
+            setIsFirstCycle(false);
+        }, startDelay);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [isFirstCycle]);
 
     const handleSelect = (selectedIndex: number) => {
         setActiveIndex(selectedIndex);
+        setIsFirstCycle(false);
     };
 
     return (
@@ -22,8 +29,8 @@ const PrizeCarousel = () => {
             <Carousel
                 activeIndex={activeIndex}
                 onSelect={handleSelect}
-                controls={false} 
-                indicators={false} 
+                controls={false}
+                indicators={false}
                 fade={true}
             >
                 {[1, 2, 3, 4, 5].map((num) => (
