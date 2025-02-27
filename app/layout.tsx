c0455ec3b12ac9e3c4ac437eaeff7f101cb714da
Owen Clarke <oc8825@princeton.edu>
2025-02-27 15:32:03 -0500

    Commenting

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,16 +7,19 @@ import type { Metadata } from "next";
 import { Open_Sans} from "next/font/google";
 import "@/styles/globals.scss";
 
+// Use Open Sans as substitute to the styling guide's Proxima Nova 
 const openSans = Open_Sans({
   subsets: ["latin"],
   variable: "--font-open-sans",
 });
 
+// Title for the tab
 export const metadata: Metadata = {
   title: "WIT Contests",
   description: "Landing Page for WIT voting contest.",
 };
 
+// Wrap children in fluid container
 export default function RootLayout({
   children,
 }: Readonly<{
--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,28 @@
 
 "use client";
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import Countdown from "@/components/Countdown";
 import PrizeCarousel from "@/components/PrizeCarousel";
-import Link from "next/link";
 import buttonStyles from "@/styles/buttons.module.scss";
 import mainStyles from "@/styles/mainSection.module.scss";
 import videoStyles from "@/styles/videoSection.module.scss";
 import ctaStyles from "@/styles/ctaSection.module.scss";
 import footerStyles from "@/styles/footerSection.module.scss";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
   return (
    <div>
-
+    {/* Create all elements in the main-section, including the countdown,
+    next contest info, title, subtitle, prize carousel, sponsor info
+    steps text, and Enter Now button*/}
     <div className={mainStyles["main-section"]}>
       <Countdown />
+
+      {/* Next contest info */}
       <div className={mainStyles["next-contest-container"]}>
         <div className={mainStyles["fresno-logo-container"]}>
           <img
@@ -41,17 +45,24 @@ export default function Home() {
           </p>
         </div>
       </div>
+
+      {/* Title */}
       <div className={mainStyles["title-container"]}>
         <p className={mainStyles["title-text"]}>
           FAN MOMENTS
         </p>
       </div>
+
+      {/* Subtitle */}
       <div className={mainStyles["subtitle-container"]}>
         <p className={mainStyles["subtitle-text"]}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pharetra congue libero in finibus. Integer nulla eros, tempus.
         </p>
       </div>
+
       <PrizeCarousel />
+
+      {/* Sponsor info */}
       <div className={mainStyles["sponsor-container"]}>
         <p className={mainStyles["sponsor-text"]}>
           PRESENTED BY<span className={mainStyles["pepsi-text"]}>&nbsp;PEPSI</span>
@@ -62,22 +73,30 @@ export default function Home() {
           className={mainStyles["pepsi-logo-image"]}
         />
       </div>
+
+      {/* Step one text */}
       <div className={mainStyles["step-one-container"]}>
         <p className={mainStyles["steps-text"]}>
           Step one id libero imperdiet, lacinia arcu ac, ullamcorper ligula. Quisque ut venenatis nulla. 
           Nulla est magna, gravida at enim eget, imperdiet lobortis.
         </p>
       </div>
+
+      {/* Step two text */}
       <div className={mainStyles["step-two-container"]}>
         <p className={mainStyles["steps-text"]}>
         Step two pulvinar, enim lacinia congue lacinia, enim quam imperdiet nisi, vel egestas tellus nisi at ex. Phasellus euismod pellentesque.
         </p>
       </div>
+
+      {/* Step three text */}
       <div className={mainStyles["step-three-container"]}>
         <p className={mainStyles["steps-text"]}>
         Step three sit amet aliquam quam faucibus sed. Proin nec ultricies libero. Vivamus sed urna.
         </p>
       </div>
+
+      {/* Enter Now button */}
       <div className={buttonStyles["larger-button-container"]}>
         <Link href="/enterNow">
           <button className={buttonStyles["larger-button"]} onClick={() => router.push("/enterNow")}>
@@ -87,10 +106,15 @@ export default function Home() {
       </div>
     </div>
 
+    {/* Create all elements in the video-section, including a caption
+    and 5 evenly spaced images */}
     <div className={videoStyles["video-section"]}>
+      {/* Caption */}
       <h3 className={videoStyles["video-caption"]}>
         HEADLINE ABOUT VIDEOS <span className={videoStyles["video-caption-red"]}>ALREADY SUBMITTED</span>
       </h3>
+
+      {/* Images */}
       <div className={videoStyles["video-container"]}>
         <img src="/images/video1.png" className={videoStyles["video-image"]} alt="Image 1" />
         <img src="/images/video2.png" className={videoStyles["video-image"]} alt="Image 2" />
@@ -100,15 +124,22 @@ export default function Home() {
       </div>
     </div>
 
+    {/* Create all elements in the cta-section, including a header, body
+    text, and another Enter Now button */}
     <div className={ctaStyles["cta-section"]}>
+      {/* Header */}
       <p className={ctaStyles["cta-header"]}>
         LAST CTA
       </p>
+
+      {/* Body text */}
       <p className={ctaStyles["cta-caption"]}>
         Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Curabitur tellus, pulvinar quis volutpat et,
         mollis vitae ligula.
       </p>
+
+      {/* Enter Now button */}
       <Link href="/enterNow">
         <button className={buttonStyles["larger-button"]} onClick={() => router.push("/enterNow")}>
           ENTER NOW
@@ -116,7 +147,11 @@ export default function Home() {
       </Link>
     </div>
 
+    {/* Create all elements in the footer section, including the WIT
+    logo, copyright statement, and buttons to the FAQ, Privacy Policy,
+    and Terms of Service Pages */}
     <div className={footerStyles["footer-section"]}>
+      {/* WIT logo */}
       <div className={footerStyles["footer-image-container"]}>
         <img
           src="/images/witLogo2x.png"
@@ -124,11 +159,15 @@ export default function Home() {
           className={footerStyles["footer-image"]}
         />
       </div>
+
+      {/* Copyright statment */}
       <div className={footerStyles["copyright-container"]}>
         <p className={footerStyles["copyright-text"]}>
           Copyright &copy; 2020 Wit. All rights reserved.
         </p>
       </div>
+
+      {/* Three buttons, with dividers between them */}
       <div className={buttonStyles["simple-button-container"]}>
         <Link href="/faq">
           <button className={buttonStyles["simple-button"]} onClick={() => router.push("/faq")}>
